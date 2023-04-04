ceb99dd3fe78fbe5b4ca7874887e93645fc3c7fd
ri-commits <kartashourychard@gmail.com>
2023-04-04 07:58:44 +0300

    Heading is complet

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { useState } from "react";
+
+const ClientOnly = () => {
+    const [hasMounted, setMounted] = useState(false);
+    return (  );
+}
+ 
+export default ClientOnly;
