21aef6e949adad88b1e4456c957b4cda03b360c0
ri-commits <kartashourychard@gmail.com>
2023-04-10 22:51:20 +0200

    bar

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -5,16 +5,16 @@ import { IoMdClose } from "react-icons/io";
 import Button from "../Button";
 
 interface ModalProps {
-  isOpen: boolean;
+  isOpen?: boolean;
   onClose: () => void;
   onSubmit: () => void;
-  title: string;
-  body: React.ReactElement;
-  footer: React.ReactElement;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
   actionLabel: string;
-  disabled: boolean;
-  secondaryAction: () => void;
-  secondaryActionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
